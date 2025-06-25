84c9666376b64f6c14a0c72939fef472ba5c0677
AvianJay <jay20090728@gmail.com>
2025-06-26 01:08:46 +0800

    google drive

---
+++ gdrive.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python
+import os
+import sys
+import time
+import json
+import argparse
+import requests
+import httplib2
+import datetime
+
+import googleapiclient
+from apiclient import discovery
+from oauth2client import client
+from selenium import webdriver
+from selenium.common import NoSuchElementException, ElementNotInteractableException
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver import Keys, ActionChains
+from selenium.webdriver.support.ui import Select
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+opt = webdriver.ChromeOptions()
+opt.add_argument("--disable-blink-features=AutomationControlled")
+opt.add_argument("--no-sandbox")
+opt.add_argument("--disable-infobars")
+opt.add_argument("--disable-dev-shm-usage")
+opt.add_argument("--disable-notifications")
+opt.add_experimental_option("excludeSwitches", ['enable-automation'])
+opt.add_argument("--app=https://accounts.google.com/embedded/setup/v2/android")
+
+AUTH_URL = "https://android.clients.google.com/auth"
+DRIVE_APPDATA_SCOPE = "https://www.googleapis.com/auth/drive.appdata"
+DRIVE_FILE_SCOPE = "https://www.googleapis.com/auth/drive.file"
+
+DEVICE_ID = "0000000000000000"
+
+GMS_SIG = "38918a453d07199354f8b19af05ec6562ced5788"
+GMS_PKG = "com.google.android.gms"
+GMS_VERSION = 11055440
+GMS_UA = "GoogleAuth/1.4 (bullhead MTC20F); gzip"
+LINE_PKG = "jp.naver.line.android"
+LINE_SIG = "89396dc419292473972813922867e6973d6f5c50"
+
+
+def get_master_token(oauth_token):
+    data = {
+        "app": GMS_PKG,
+        "client_sig": GMS_SIG,
+        "google_play_services_version": GMS_VERSION,
+        "androidId": DEVICE_ID,
+        "lang": "en_US",
+        "ACCESS_TOKEN": "1",
+        "Token": oauth_token,
+        "service": "ac2dm",
+    }
+
+    headers = {
+        "Content-type": "application/x-www-form-urlencoded",
+        "User-Agent": GMS_UA,
+        "device": DEVICE_ID,
+        "Connection": "close",
+    }
+
+    r = requests.post(AUTH_URL, headers=headers, data=data)
+    r.raise_for_status()
+
+    token = None
+    lines = r.text.split("\n")
+    for l in lines:
+        if l.startswith("Token"):
+            token = l.split("=")[1].strip()
+            break
+
+    return token
+
+
+def get_gdrive_access_token(account, master_token, app_id, app_sig):
+    requestedService = "oauth2: https://www.googleapis.com/auth/drive.appdata"
+
+    data = {
+        "androidId": DEVICE_ID,
+        "lang": "en_US",
+        "google_play_services_version": GMS_VERSION,
+        "sdk_version": 23,
+        "device_country": "us",
+        "is_called_from_account_manager": 1,
+        "client_sig": app_sig,
+        "callerSig": GMS_SIG,
+        "Email": account,
+        "has_permission": 1,
+        "service": requestedService,
+        "app": app_id,
+        "check_email": 1,
+        "token_request_options": "CAA4AQ==",
+        "system_partition": 1,
+        "_opt_is_called_from_account_manager": 1,
+        "callerPkg": GMS_PKG,
+        "Token": master_token,
+    }
+
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Connection": "close"}
+
+    r = requests.post(AUTH_URL, headers=headers, data=data)
+    r.raise_for_status()
+
+    token = None
+    lines = r.text.split("\n")
+    for l in lines:
+        if l.startswith("Auth"):
+            token = l.split("=")[1].strip()
+            break
+
+    return token
+
+
+def get_gdrive_service(gdrive_token):
+    credentials = client.AccessTokenCredentials(gdrive_token, "Mozilla/5.0 compatible")
+    credentials.scopes.add(DRIVE_FILE_SCOPE)
+    credentials.scopes.add(DRIVE_APPDATA_SCOPE)
+
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build("drive", "v3", http=http)
+
+    return service
+
+
+def download_files(service, app_name):
+    result = (
+        service.files()
+        .list(
+            spaces="appDataFolder",
+            fields="nextPageToken, files(id, name, modifiedTime)",
+            q="not trashed",
+            pageSize=1000,
+        )
+        .execute()
+    )
+
+    files = result.get("files", [])
+    nextPageToken = result.get("nextPageToken")
+
+    while nextPageToken:
+        result = (
+            service.files()
+            .list(
+                spaces="appDataFolder",
+                pageSize=1000,
+                pageToken=nextPageToken,
+                fields="nextPageToken, files(id, name, modifiedTime)",
+            )
+            .execute()
+        )
+
+        files.extend(result.get("files", []))
+        nextPageToken = result.get("nextPageToken")
+
+    if not files:
+        print("No files found")
+        return 0
+
+    print(f"Found {len(files)} files, starting download")
+
+    output_dir = f"{app_name}-{int(time.time())}"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for file_item in files:
+        print(f"Downloading file {file_item['name']} with id {file_item['id']}")
+        req = service.files().get_media(fileId=file_item["id"])
+        output_path = os.path.join(output_dir, f"{file_item['name']}")
+        with open(output_path, "wb") as f:
+            downloader = googleapiclient.http.MediaIoBaseDownload(f, req)
+            done = False
+            while done is False:
+                _status, done = downloader.next_chunk()
+
+        modified_time = datetime.datetime.fromisoformat(file_item["modifiedTime"]).timestamp()
+        os.utime(output_path, (modified_time, modified_time))
+
+    return 0
+
+def browser_get_oauth_token(email=None):
+    # if os.path.exists(".googleoauth") and not force:
+    #     return open(".googleoauth", "r").read()
+    driver = webdriver.Chrome(options=opt)
+    if email:
+        driver.find_element(By.CSS_SELECTOR, "input[type=email]").send_keys(email, Keys.ENTERssssss)
+    while not driver.get_cookie("oauth_token"):
+        time.sleep(.5)
+    time.sleep(2)
+    token = driver.get_cookie("oauth_token").get("value")
+    print("Got OAuth Token")
+    print(token)
+    open(".googleoauth", "w").write(token)
+    driver.quit()
+    return token
+
+def all(email):
+    auth = None
+    if os.path.exists(".googleauth.json"):
+        auths = json.load(open(".googleauth.json", "r"))
+        if auths.get(email):
+            auth = auths.get(email)
+    else:
+        auths = {}
+    if not auth:
+        oauthtoken = browser_get_oauth_token(email)
+        mastertoken = get_master_token(oauthtoken)
+        gdrive_token = get_gdrive_access_token(email, mastertoken, LINE_PKG, LINE_SIG)
+        auths[email] = {
+            "oauth": oauthtoken,
+            "master": mastertoken,
+            "gdrive": gdrive_token,
+        }
+    json.dump(auths, open(".googleauth.json", "w"))
+    service = get_gdrive_service(auths[email]["gdrive"])
+    download_files(service, LINE_PKG)
