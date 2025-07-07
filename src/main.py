86f490a072eb9bd0edfbfdd94f7719bb80f7e813
AvianJay <jay20090728@gmail.com>
2025-07-07 18:16:06 +0800

    update

--- src/main.py
+++ src/main.py
@@ -7,6 +7,28 @@ import convert
 def main(page: ft.Page):
     page.title = "LINETransfer"
 
+    convert_column = ft.Column(controls=[
+        ft.Text("Convert View")
+    ])
+
+    tools_column = ft.Column(controls=[
+        ft.Text("Tools View")
+    ])
+
+    settings_column = ft.Column(controls=[
+        ft.Text("Settings View")
+    ])
+
+    def rail_on_change(e):
+        vr.controls.pop(-1)
+        if e.control.selected_index == 0:
+            vr.controls.append(convert_column)
+        elif e.control.selected_index == 1:
+            vr.controls.append(tools_column)
+        elif e.control.selected_index == 2:
+            vr.controls.append(settings_column)
+        page.update()
+
     rail = ft.NavigationRail(
         selected_index=0,
         label_type=ft.NavigationRailLabelType.ALL,
@@ -18,26 +40,30 @@ def main(page: ft.Page):
         group_alignment=-0.9,
         destinations=[
             ft.NavigationRailDestination(
-                icon=ft.Icons.ANDROID_OUTLINED,
-                selected_icon=ft.Icons.ANDROID,
-                label="轉Android",
+                icon=ft.Icons.SYNC_OUTLINED,
+                selected_icon=ft.Icons.SYNC,
+                label="轉換",
             ),
+            # ft.NavigationRailDestination(
+            #     icon=ft.Icons.APPLE_OUTLINED,
+            #     selected_icon=ft.Icons.APPLE,
+            #     label="轉iOS",
+            # ),
             ft.NavigationRailDestination(
-                icon=ft.Icons.APPLE_OUTLINED,
-                selected_icon=ft.Icons.APPLE,
-                label="轉iOS",
+                icon=ft.Icons.HANDYMAN_OUTLINED,
+                selected_icon=ft.Icons.HANDYMAN,
+                label="工具",
             ),
             ft.NavigationRailDestination(
                 icon=ft.Icons.SETTINGS_OUTLINED,
-                selected_icon=ft.Icon(ft.Icons.SETTINGS),
+                selected_icon=ft.Icons.SETTINGS,
                 label="設定",
             ),
         ],
-        on_change=lambda e: print("Selected destination:", e.control.selected_index),
+        on_change=rail_on_change,
     )
-
-    page.add(
-        ft.Row(
+    
+    vr = ft.Row(
             [
                 rail,
                 ft.VerticalDivider(width=1),
@@ -49,7 +75,7 @@ def main(page: ft.Page):
             ],
             expand=True,
         )
-    )
+    page.add(vr)
 
 
 ft.app(main)
