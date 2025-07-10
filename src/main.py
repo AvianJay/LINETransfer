8bdddc28aa297aaa8575c04c454cd38285eb6db8
AvianJay <jay20090728@gmail.com>
2025-07-10 23:10:48 +0800

    center

--- src/main.py
+++ src/main.py
@@ -413,61 +413,77 @@ def main(page: ft.Page):
         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
     )
 
-    tools_column = ft.Column(controls=[
-        ft.Text("Tools Page"),
-    ])
+    tools_column = ft.Column(
+        controls=[
+            ft.Container(
+                expand=True,
+                content=ft.Text(
+                        "¯\\_(ツ)_/¯\n空空如也",
+                        text_align=ft.TextAlign.CENTER,
+                        size=30
+                    ),
+                alignment=ft.alignment.center,
+            )
+        ],
+        alignment=ft.MainAxisAlignment.CENTER,
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+    )
 
-    settings_column = ft.Column(controls=[
-        ft.Text("Settings Page"),
-        ft.Text("主題設定"),
-        ft.Dropdown(
-            label="主題",
-            options=[
-                ft.DropdownOption(
-                    key="system",
-                    leading_icon=ft.Icons.BRIGHTNESS_AUTO,
-                    text="跟隨系統",
-                    content=ft.Text("跟隨系統"),
-                ),
-                ft.DropdownOption(
-                    key="light",
-                    leading_icon=ft.Icons.LIGHT_MODE,
-                    text="淺色",
-                    content=ft.Text("淺色"),
-                ),
-                ft.DropdownOption(
-                    key="dark",
-                    leading_icon=ft.Icons.DARK_MODE,
-                    text="深色",
-                    content=ft.Text("深色"),
-                ),
-            ],
-            on_change=lambda e: update_theme(e.control.value),
-            value=config.config("theme"),
-        ),
-        # config.config("ios_backup_location")
-        ft.Text("iOS裝置備份位置"),
-        ft.TextField(
-            label="備份位置",
-            value=config.config("ios_backup_location"),
-            on_change=lambda e: config.config("ios_backup_location", e.control.value, "w"),
-            hint_text="iDeviceBackups",
-            helper_text="請確保這個資料夾已經存在。",
-        ),
-        ft.Text("應用程式更新檢查設定"),
-        ft.Dropdown(
-            label="自動更新方式",
-            options=[
-                ft.DropdownOption(key="no", text="不提示更新", content=ft.Text("不提示更新")),
-                ft.DropdownOption(key="popup", text="彈出更新提示", content=ft.Text("彈出更新提示")),
-                ft.DropdownOption(key="notify", text="通知更新", content=ft.Text("通知更新")),
-            ],
-            on_change=lambda e: config.config("app_update_check", e.control.value, "w"),
-            value=config.config("app_update_check"),
-        ),
-        ft.Text(f"App Version: {config.app_version}"),
-        ft.Text(f"Update Channel: {config.update_channel}"),
-    ])
+    settings_column = ft.Column(
+        controls=[
+            ft.Text("應用程式設定", size=25),
+            ft.Text("主題設定", size=20),
+            ft.Dropdown(
+                label="主題",
+                options=[
+                    ft.DropdownOption(
+                        key="system",
+                        leading_icon=ft.Icons.BRIGHTNESS_AUTO,
+                        text="跟隨系統",
+                        content=ft.Text("跟隨系統"),
+                    ),
+                    ft.DropdownOption(
+                        key="light",
+                        leading_icon=ft.Icons.LIGHT_MODE,
+                        text="淺色",
+                        content=ft.Text("淺色"),
+                    ),
+                    ft.DropdownOption(
+                        key="dark",
+                        leading_icon=ft.Icons.DARK_MODE,
+                        text="深色",
+                        content=ft.Text("深色"),
+                    ),
+                ],
+                on_change=lambda e: update_theme(e.control.value),
+                value=config.config("theme"),
+            ),
+            # config.config("ios_backup_location")
+            ft.Text("iOS裝置備份位置", size=20),
+            ft.TextField(
+                label="備份位置",
+                value=config.config("ios_backup_location"),
+                on_change=lambda e: config.config("ios_backup_location", e.control.value, "w"),
+                hint_text="iDeviceBackups",
+                helper_text="請確保這個資料夾已經存在。",
+            ),
+            ft.Text("應用程式更新檢查設定", size=20),
+            ft.Dropdown(
+                label="自動更新方式",
+                options=[
+                    ft.DropdownOption(key="no", text="不提示更新", content=ft.Text("不提示更新")),
+                    ft.DropdownOption(key="popup", text="彈出更新提示", content=ft.Text("彈出更新提示")),
+                    ft.DropdownOption(key="notify", text="通知更新", content=ft.Text("通知更新")),
+                ],
+                on_change=lambda e: config.config("app_update_check", e.control.value, "w"),
+                value=config.config("app_update_check"),
+            ),
+            ft.Text(f"App Version: {config.app_version}"),
+            ft.Text(f"Update Channel: {config.update_channel}"),
+        ],
+        alignment=ft.MainAxisAlignment.CENTER,
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+    )
 
     def home_show_page(index):
         vr.controls.pop(-1)
@@ -478,9 +494,17 @@ def main(page: ft.Page):
                 alignment=ft.alignment.center,
             ))
         elif index == 1:
-            vr.controls.append(tools_column)
+            vr.controls.append(ft.Container(
+                content=tools_column,
+                expand=True,
+                alignment=ft.alignment.center,
+            ))
         elif index == 2:
-            vr.controls.append(settings_column)
+            vr.controls.append(ft.Container(
+                content=settings_column,
+                expand=True,
+                alignment=ft.alignment.center,
+            ))
         page.update()
 
     def rail_on_change(e):
