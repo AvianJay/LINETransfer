69b5f7cd4163deb259366ee1549e5089993814b6
AvianJay <jay20090728@gmail.com>
2025-07-10 22:18:56 +0800

    add progress callback

--- src/sparserestore/__init__.py
+++ src/sparserestore/__init__.py
@@ -8,13 +8,13 @@ from pymobiledevice3.exceptions import PyMobileDevice3Exception
 from . import backup
 from .backup import _FileMode as FileMode
 
-def perform_restore(backup: backup.Backup, reboot: bool = False):
+def perform_restore(backup: backup.Backup, reboot: bool = False, progress_callback=lambda x: None):
     with TemporaryDirectory() as backup_dir:
         backup.write_to_directory(Path(backup_dir))
             
         lockdown = create_using_usbmux()
         with Mobilebackup2Service(lockdown) as mb:
-            mb.restore(backup_dir, system=True, reboot=False, copy=False, source=".")
+            mb.restore(backup_dir, system=True, reboot=False, copy=False, source=".", progress_callback=progress_callback)
 
 def exploit_write_file(file: backup.BackupFile):
     # Exploits in use:
